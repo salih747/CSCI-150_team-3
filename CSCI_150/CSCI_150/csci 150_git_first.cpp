6b61ae1b5e19698e7671feba7057c97bc1c9dacd
salih747 <salih747@mail.fresnostate.edu>
2017-09-18 18:46:58 -0700

    Update csci 150_git_first.cpp

--- CSCI_150/CSCI_150/csci 150_git_first.cpp
+++ CSCI_150/CSCI_150/csci 150_git_first.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
 
 	cout << "Hello this is team 3 of CSCI 150 class" << endl;
+	cout << "Hello team, I'm so excited to work with ya'll :)" << endl;
 
 	system("pause");
 	return 0;
